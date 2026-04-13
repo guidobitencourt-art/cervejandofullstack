40af8d0f8aca3534a9e7b604b4f8142eb8699beb
Guilherme Bitencourt <guidobitencourt@alumnos.cei.es>
2026-04-13 21:47:16 +0200

    Add Hero component with styles and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Banner from "./components/Banner/Banner"
+import Hero from "./components/Hero/Hero"
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Banner />
+      <Hero />
       <Footer />
       
     </>
